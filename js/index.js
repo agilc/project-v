e7158641b1a710d34fe43a238e9e65c4124223af
agilc <agilc315@gmail.com>
2017-04-06 19:02:31 +0530

    before redirect

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 angular.module('myApp', [])
-.controller('myCtrl', ['$scope', function($scope) {
+.controller('myCtrl', ['$scope', function($scope,$window) {
     $scope.count = 0;
+    var loginData=this;
     $scope.myFunc = function() {
         $scope.count++;
-        alert();
+        alert(this.name);
+        $window.location.href = "https://www.google.co.in/" ;
     };
 }]);
